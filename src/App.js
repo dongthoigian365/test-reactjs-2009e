d2a90e2b361374abf947f10469614ac30413c0b5
dongthoigian365 <37582882+dongthoigian365@users.noreply.github.com>
2020-11-25 22:54:42 +0700

    Uploat ex1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,24 @@ class App extends React.Component{
   render() {
     return (
       <div>
-        <h1>Tao Component bang react class</h1>
+        <form action="" method="get">
+            <div id="header">
+                <h1>Reactjs Quiz</h1>
+                <progress id="file" value="0" max="100"> 32% </progress>
+                <label for="file">0% complete</label>
+            </div>
+            <div id="main">
+                <h2>What is the full form of HTTP?</h2>
+                <p>a. Hyper text transfer package</p>
+                <p>b. Hyper text trabsfer protocol</p>
+                <p>c. Hyphenation text test program</p>
+                <p>d. None of the above</p>
+            </div>
+            <div id="footer">
+                <input type="button" value="Back"/>
+                <input type="button" value="Skip"/>
+            </div>
+        </form>
       </div>
     )
   }
