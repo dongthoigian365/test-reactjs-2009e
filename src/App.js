aa9f7364f37238d6ab8680fbe951992ff19e63b7
dongthoigian365 <37582882+dongthoigian365@users.noreply.github.com>
2020-11-25 23:36:38 +0700

    Upload ex2

--- src/App.js
+++ src/App.js
@@ -5,24 +5,37 @@ class App extends React.Component{
   render() {
     return (
       <div>
-        <form action="" method="get">
-            <div id="header">
-                <h1>Reactjs Quiz</h1>
-                <progress id="file" value="0" max="100"> 32% </progress>
-                <label for="file">0% complete</label>
-            </div>
-            <div id="main">
-                <h2>What is the full form of HTTP?</h2>
-                <p>a. Hyper text transfer package</p>
-                <p>b. Hyper text trabsfer protocol</p>
-                <p>c. Hyphenation text test program</p>
-                <p>d. None of the above</p>
-            </div>
-            <div id="footer">
-                <input type="button" value="Back"/>
-                <input type="button" value="Skip"/>
-            </div>
-        </form>
+        <ul>
+        <li>
+            <a href="">
+                <i class="fas fa-home"></i>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-flask"></i>
+                <p>Projects</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-angle-double-right"></i>
+                <p>Breadcrumb</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-rocket"></i>
+                <p>Getting started</p>
+            </a>
+        </li>
+        <li>
+            <a href="">
+                <i class="fas fa-arrow-down"></i>
+                <p>Download</p>
+            </a>
+        </li>
+    </ul>
       </div>
     )
   }
